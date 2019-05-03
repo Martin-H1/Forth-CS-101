d8fdc2845c82148c88906086ccab261e099097d2
Martin-H1 <mheermance@gmail.com>
2019-05-03 19:50:13 -0400

    fizzbuzz solutions

---
+++ fizzbuzz1.fs
@@ -0,0 +1,26 @@
+; I watched this Tom Scott video: https://www.youtube.com/watch?v=QPZ0pIK_wsc
+; and realized it was the perfect example of a CS-101 Forth problem. So I did
+; it two different ways. This is the more straight foreward way:
+
+: FIZZ?  ( n -- flag)
+  3 MOD 0= ;
+
+: BUZZ?   ( n -- flag)
+  5 MOD 0= ;
+
+: .FIZZ   ( n -- flag)
+  FIZZ? DUP
+  IF ." Fizz " THEN ;
+
+: .BUZZ   ( n -- flag)
+  BUZZ? DUP
+  IF ." Buzz " THEN ;
+
+: FIZZBUZZ   ( --)
+  101 1 DO
+    I .FIZZ I .BUZZ
+    OR 0= IF I . THEN
+    CR
+  LOOP ;
+
+FIZZBUZZ
---
+++ fizzbuzz2.fs
@@ -0,0 +1,31 @@
+; I watched this Tom Scott video: https://www.youtube.com/watch?v=QPZ0pIK_wsc
+; and realized it was the perfect example of a CS-101 Forth problem. So I did
+; it two different ways. This is approach was inspired by nyef in the 6502
+; forum. He indexed into a list of execution tokens based upon the remainder.
+; The token determines the output, and most of them are the TOS, but the
+; exceptions output text and drop TOS. It's extensible just by changing the
+; execution token table. I tweaked it to use create does>, although as nyef
+; pointed out, my usage isn't ANS standard Forth.
+
+: .FIZZ ( unused --)
+  ." Fizz " DROP ;
+
+: .BUZZ ( unused --)
+  ." Buzz " DROP ;
+
+: .FIZZBUZZ ( unused --)
+  ." Fizz Buzz " DROP ;
+
+CREATE BEES
+  ' .FIZZBUZZ , ' . , ' . , ' .FIZZ , ' . ,
+  ' .BUZZ , ' .FIZZ , ' . , ' . , ' .FIZZ ,
+  ' .BUZZ , ' . , ' .FIZZ , ' . , ' . ,
+DOES>  ( i addr --)
+  SWAP TUCK
+  15 MOD CELLS + @ EXECUTE ;
+
+: FIZZBUZZ
+  101 1 DO
+    I BEES
+    CR
+  LOOP ;
