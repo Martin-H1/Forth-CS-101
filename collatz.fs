afcf997f67903d2753df137c74cc83a75bfae373
Martin-H1 <mheermance@gmail.com>
2019-05-03 20:08:49 -0400

    Update collatz.fs

--- collatz.fs
+++ collatz.fs
@@ -1,5 +1,5 @@
-; After watching this video: https://youtu.be/5mFpVDpKX70 on the Collatz conjecture,
-; I decided to Forth CS-101 it!
+\ After watching this video: https://youtu.be/5mFpVDpKX70 on the Collatz conjecture,
+\ I decided to Forth CS-101 it!
 
 : odd? dup 1 and ;
 : 3n+1 dup 2* + 1 + ;
@@ -8,8 +8,7 @@
 : collatz begin .n dup 1 = invert while collatz(n) repeat cr drop ;
 
 15 collatz
-; Produces this output: 15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1 
+\ Produces this output: 15 46 23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1 
 
 1024 collatz
-
-; Produces this output: 1024 512 256 128 64 32 16 8 4 2 1
+\ Produces this output: 1024 512 256 128 64 32 16 8 4 2 1
